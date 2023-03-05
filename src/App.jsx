7ece1d504a53bb80ef394cbfcd22ddd4351baea1
Op Pautu <pautuop@gmail.com>
2023-03-05 17:03:48 +0530

    Refactor code by extracting component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
-import { FaQuoteRight } from "react-icons/fa";
+import Slide from "./Slide";
 import data from "./data";
 
 function App() {
@@ -47,15 +47,7 @@ function App() {
             position = "lastSlide";
           }
 
-          return (
-            <article className={position} key={id}>
-              <img src={image} alt={name} className="person-img" />
-              <h4>{name}</h4>
-              <p className="title">{title}</p>
-              <p className="text">{quote}</p>
-              <FaQuoteRight className="icon" />
-            </article>
-          );
+          return <Slide key={person.id} person={person} position={position} />;
         })}
         <button className="prev" onClick={() => setIndex(index - 1)}>
           <FiChevronLeft />
